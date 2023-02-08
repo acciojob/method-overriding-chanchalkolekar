c74d819421660023e63623d377261b4ed33af113
chanchal kolekar <chanchalkolekar3@gmail.com>
2023-02-09 03:39:45 +0530

    Upadting the class inside main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B tempb = new B();
+        tempb.meth();
+
+        B tempbb = new B();
+        tempbb.meth();
+    }
 }
